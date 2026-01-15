91e76aca873c8d9e4acf0023274ac20a1f320a02
Alexandra Molsberger <alexandra.molsberger@study.hs-duesseldorf.de>
2026-01-15 16:07:26 +0100

    added test_fair_sharer()

--- tests/test_fairsharer.py
+++ tests/test_fairsharer.py
@@ -1,10 +1,9 @@
 """Tests for the fairsharer.my_module module.
 """
-import pytest
 
 from fairsharer.fair_sharer import fair_sharer
 
-
+vals = [10, 100, 0, 900]
 def test_fair_sharer():
-    pass
+    assert fair_sharer(vals, 1) == [100, 100, 90, 720]
     
